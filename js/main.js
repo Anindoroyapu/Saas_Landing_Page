78d37c1b411a3d6291c54b98bbc2818d2b24e135
Anindo Roy <142706157+Anindoroyapu@users.noreply.github.com>
2023-11-02 15:11:57 +0600

    JavaScript file update

---
+++ js/main.js
@@ -0,0 +1,27 @@
+//FAQ According
+document.addEventListener('DOMContentLoaded',() =>{
+    const faqContainer = document.querySelector('.faq-content');
+
+    faqContainer.addEventListener('click', (e) => {
+        const groupHeader = e.target.closest('.faq-group-header');
+
+        if (!groupHeader) return;
+
+        const group = groupHeader.parentElement;
+        const groupBody = group.querySelector('faq-group-body');
+        const icon = groupHeader.querySelector('i');
+
+        // Toggle icon
+        icon.classList.toggle('fa-plus');
+        icon.classList.toggle('fa-minus');
+
+        //Toggle visiblity of body
+        groupBody.classList.toggle('open');
+
+        // Close other open FAQ bodies
+        const otherGrourps = faqContainer.querySelectorAll('faq-group');
+        otherGrourps.forEach((otherGrourp) => {
+            
+        })
+         });
+});
