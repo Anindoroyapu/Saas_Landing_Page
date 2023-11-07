1ff5bef534cfde7ec322ceb080a1511b64ab8da1
Anindo Roy <142706157+Anindoroyapu@users.noreply.github.com>
2023-11-07 21:06:53 +0600

    Footer Complete

--- js/main.js
+++ js/main.js
@@ -19,10 +19,18 @@ document.addEventListener('DOMContentLoaded',() =>{
         groupBody.classList.toggle('open');
 
         // Close other open FAQ bodies
-        const otherGrourps = faqContainer.querySelectorAll('faq-group');
+        const otherGrourps = faqContainer.querySelectorAll('.faq-group');
+
         otherGrourps.forEach((otherGrourp) => {
-           
+           if (otherGrourp !== group){
+            const otherGrourpBody = otherGrourp.querySelector('.faq-group-body');
+            const otherIcon = otherGrourp.querySelector('.faq-group-header i');
+            otherGrourpBody.classList.remove('open');
+            otherIcon.classList.remove('fa-minus');
+            otherIcon.classList.add('fa-plus');
+
+           }; 
             
-        })
+        });
          });
 });
